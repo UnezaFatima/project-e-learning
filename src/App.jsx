f801e2632f56badfa9f018a40113880bd0ab1d61
UnezaFatima <124772635+UnezaFatima@users.noreply.github.com>
2024-11-15 04:53:04 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./App.css";
-// import Navbar2 from './components/navbar/components/Nav2.jsx';
-// import Component1 from "./components/literature/components/Component1";
-// import Component2 from "./components/literature/components/Component2";
-// import Component3 from './components/literature/components/Component3'
-// import Footer from './components/footer/Footer.jsx';
-// import Login from './components/navbar/components/Login.jsx';
+import Navbar2 from './components/navbar/components/Nav2.jsx';
+import Component1 from "./components/literature/components/Component1";
+import Component2 from "./components/literature/components/Component2";
+import Component3 from './components/literature/components/Component3'
+import Footer from './components/footer/Footer.jsx';
+import Login from './components/navbar/components/Login.jsx';
+import Meeting from "./components/other_section/Meeting/Meeting";
+
 
 function App(){
   return (
     <>
     
-      {/* <Navbar2 />
+      <Navbar2 />
       <Component1/>
       <Component2 />
       <Component3 />
       <Footer/>
-      <Login/> */}
+      <Login/>
+      <Meeting />
+      
           
 
       {/* <div>
